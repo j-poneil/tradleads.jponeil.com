f08d4109322f066879c48b21cd7f01f9d89a6827
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-02-13 17:12:06 -0800

    Starting fresh with create-react-app, react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import { About } from '';
+import { List } from '';
+import { Graph } from '';
+import { Gallery } from '';
+import { Progress } from '';
+import { Error404 } from '';
+// import { About } from '';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/list">List</Link></li>
+          <li><Link to="/graph">Graph</Link></li>
+          <li><Link to="/gallery">Gallery</Link></li>
+          <li><Link to="/amga-progress">AMGA Progress</Link></li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route path="/list">
+          <List />
+        </Route>
+        <Route path="/graph">
+          <Graph />
+        </Route>
+        <Route path="/gallery">
+          <Gallery />
+        </Route>
+        <Route path="/amga-progress">
+          <Progress />
+        </Route>
+        {/* <Route path="/about">
+          <About />
+        </Route> */}
+        <Route path="*">
+          <Error404 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
