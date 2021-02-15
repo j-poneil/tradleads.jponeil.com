04c3e3a7c9d03125d320c6879222c7d5ddce0cf0
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-02-15 12:33:20 -0800

    Now able to pull in SQL DB list as JSON.

--- src/routes/List.js
+++ src/routes/List.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+/*
+! Probably wont see anything...
+! because this is local live reload, which doesn't use PHP at all! Doh!
+*/
 
 export const List = () => {
+    const [ data, setData ] = useState('no data yet');
+    let phpFormattedResponse = 'none';
+
+    useEffect(() => {
+        //effect
+
+        // TODO - This works for getting result as JSON and updating DOM
+        let oReq = new XMLHttpRequest(); //New request object
+		oReq.onload = function(){
+            //This is where you handle what to do with the response.
+            //The actual data is found on this.responseText
+            //this.responseText will be json_encode($result)
+            setData(this.responseText);
+        };
+        // true... true here makes it so that it doesn't block the rest of the execution
+        oReq.open("get", "../../getJSONtable.php", true);
+        oReq.send();
+
+        return () => {
+            //cleanup
+        }
+    }, [])
+
+
+
+
     return (
         <div>
-            List of leads...
+            List of leads...<br/>
+            { data }
         </div>
     )
 }
