584f30b1f24023448ebc12b40cb6dcd9c5612a73
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-02-16 09:36:40 -0800

    Expanded the php so it can run pre-set SQL queries

--- getJSONtable.php
+++ getJSONtable.php
@@ -13,8 +13,57 @@ if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
 
-$sql = "SELECT * FROM `TABLE 1`";
-$result = mysqli_query($con,$sql);
+// Originally used these 2 lines:
+// $sql = "SELECT * FROM `TABLE 1`";
+// $result = mysqli_query($con,$sql);
+
+// NOW
+$whichTable = $_GET['q'];
+// later have this point to stored procedures
+// options -- 'default', 'default_rev', 'pitches', 'pitches_rev', 'area', 'area_rev', 'difficulty', 'difficulty_rev', 'other'
+switch($whichTable){
+	case 'default':
+		$sql = "SELECT * FROM `TABLE 1`";
+		$result = mysqli_query($con,$sql);
+		break;
+	case 'default_rev':
+		$sql = "SELECT * FROM `TABLE 1` ORDER BY `#` DESC";
+		$result = mysqli_query($con,$sql);
+		break;
+	case 'pitches':
+		$sql = "SELECT * FROM `TABLE 1` ORDER BY `pitches` DESC, `name` ASC";
+		$result = mysqli_query($con,$sql);
+		break;
+	case 'pitches_rev':
+		$sql = "SELECT * FROM `TABLE 1` ORDER BY `pitches` ASC, `name` ASC";
+		$result = mysqli_query($con,$sql);
+		break;
+	case 'area':
+		$sql = "SELECT * FROM `TABLE 1` ORDER BY `area` ASC, `name` ASC";
+		$result = mysqli_query($con,$sql);
+		break;
+	case 'area_rev':
+		$sql = "SELECT * FROM `TABLE 1` ORDER BY `area` DESC, `name` ASC";
+		$result = mysqli_query($con,$sql);
+		break;
+	case 'difficulty':
+		$sql = "SELECT * FROM `TABLE 1` ORDER BY `diff_sort` DESC, `pitches` ASC";
+		$result = mysqli_query($con,$sql);
+		break;
+	case 'difficulty_rev':
+		$sql = "SELECT * FROM `TABLE 1` ORDER BY `diff_sort` ASC, `pitches` DESC";
+		$result = mysqli_query($con,$sql);
+		break;
+	case 'other':
+		$sql = "SELECT * FROM `TABLE 1`";
+		$result = mysqli_query($con,$sql);
+		break;
+	default:
+		$sql = "SELECT * FROM `TABLE 1`";
+		$result = mysqli_query($con,$sql);
+		break;
+}
+
 
 $tableArray = array();
 // While loop is necissary to go through ALL the data from the SQL query
