f28eefccfcbdd397f1370d3f5e86b376751b73d4
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-02-25 14:22:34 -0800

    Title component, to be changed and styled soon

---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Title = () => {
+    const StyledTitle = styled.h1``;
+    
+    return (
+        <StyledTitle>Title of The Page</StyledTitle>
+    )
+}
