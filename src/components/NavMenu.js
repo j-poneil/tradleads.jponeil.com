cc8774696c2c4bdd7d049dbc94795c3c21c3e246
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-02-24 23:15:14 -0800

    Split StyledLi into its own component, added notes

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import { device } from './../breakpoints/breakpoints';
 
-export const NavMenu = () => {
+// What am I trying to do with this?
+// OK... So... This component is the styled <nav></nav> with styled <ul></ul> inside it, which will be used to wrap the children list elements, which will also be a styled component, <NavMenuItem/>. In my use case I will not need anything else here. This is just a place to do styling instead of in CSS.
+// I'm thinking of the overall positioning being done outside in App.css or something...
+
+export const NavMenu = (props) => {
     const StyledNav = styled.nav`
+        /* temp border*/
+        border: 2px solid white; 
     `;
 
     const StyledUl = styled.ul`
-    `;
-
-    const StyledLi = styled.li`
+        /* temp border*/
+        border: 2px solid pink;
     `;
 
     return (
         <StyledNav>
             <StyledUl>
-                <StyledLi />
+                { props.children }
             </StyledUl>
         </StyledNav>
     )
